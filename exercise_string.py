6c8b449f8e0f4116afaf2d1636993c56c70b8742
ravikrranjan <raviranjan1600@gmail.com>
2020-03-27 22:10:34 +0530

    refactor code

--- exercise_string.py
+++ exercise_string.py
@@ -1,8 +1,8 @@
 # create a program that asks the user for his first name , his middle name and his last name. then print
 
-firstName = input("Please Enter your first name: ")
-middleName = input("Pleae enter your middle name: ")
-lastName = input("Please enter your last name: ")
+firstName = input("Enter your first name: ")
+middleName = input("Enter your middle name: ")
+lastName = input("Enter your last name: ")
 
 print("Your initilas are " + firstName[0] +
       " " + middleName[0] + " " + lastName[0])

999d41d770ea03069c10494a6fbc49144c948272
Ravi <rkumar792@gmail.com>
2020-03-27 21:38:35 +0530

    get substring

--- dataType_string.py
+++ dataType_string.py
@@ -18,3 +18,34 @@ print(myString[0])
 
 # minus index read string from last
 print(myString[-1], '----', myString[:-2:1])
+
+
+# Get lenght of string by len function
+
+ln = len(myString)
+
+print(ln)
+
+# get last character of string using its length
+
+myString = "Ravi"
+ln = len(myString)
+print(myString[ln - 1])
+
+# bank ifsc code
+ifscCode = "SBIN0003456"
+
+# get bank name
+
+print(ifscCode[0:3])
+
+# bank ifsc code
+ifscCode = "0003456SBI"
+
+# get bank name
+# read from -3 to last index of string
+print(ifscCode[-3:])
+
+# begining of string
+
+print(myString[:2])

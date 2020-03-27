f0d51d926db2624f4f9ddd989c468de5578276fe
Ravi <rkumar792@gmail.com>
2020-03-27 21:27:23 +0530

    string index and typeof

--- dataType_string.py
+++ dataType_string.py
@@ -1,6 +1,20 @@
 myName = "Ravi"
 
-print(type (myName)) #Result: <class 'str'> 
+print(type(myName))  # Result: <class 'str'>
 
 str1 = 'Ravi'
-print(type(str1)) #Result:  <class 'str'> 
+print(type(str1))  # Result:  <class 'str'>
+
+# String should always be inside quotation marks event if you pass a number inside quotations marks
+
+mystring = "33"
+
+print(type(mystring))
+
+# every string is a sequence of character
+myString = 'meet at 5pm'
+
+print(myString[0])
+
+# minus index read string from last
+print(myString[-1], '----', myString[:-2:1])

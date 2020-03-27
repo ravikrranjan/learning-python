7c41ac8bc5068b167923e80943bfaef818b631fa
ravikrranjan <raviranjan1600@gmail.com>
2020-03-27 22:09:20 +0530

    s

--- exercise_string.py
+++ exercise_string.py
@@ -11,4 +11,4 @@ productNumber = "037-00901-00027"
 
 print("Country code: " + str(productNumber[:3]))
 print("Product code: " + productNumber[4:9])
-print("Batch number: " + productNumber[-5:])
+print("Batch number:" + productNumber[-5:])

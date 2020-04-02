6d709568c5b31216fa12b065981afd611e050a54
ravikrranjan <raviranjan1600@gmail.com>
2020-04-02 22:19:17 +0530

    basic error handling

---
+++ error.py
@@ -0,0 +1,19 @@
+
+number = input('Type a number: ')
+try:
+    number = float(number)
+    print('The number is : ', number)
+except:
+    print('Invalid number')
+
+for i in range(10):
+    print('Hello world')
+
+
+try:
+    inp = input()
+    print('Press Ctrl+C or Interrupt the Kernel:')
+except KeyboardInterrupt:
+    print('Caught KeyboardInterrupt')
+else:
+    print('No exception occurred')

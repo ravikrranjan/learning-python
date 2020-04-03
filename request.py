2bbd8a4b079ed328e8aa2a53410c88b979e81462
ravikrranjan <raviranjan1600@gmail.com>
2020-04-03 22:37:46 +0530

    basic requsts

--- request.py
+++ request.py
@@ -1,7 +1,13 @@
+import pprint
 import requests
-
+import json
 res = requests.get('https://google.com')
 
-print(res.status_code)
-print(res.headers['Date'])
-print(res.text)
+# print(res.status_code)
+# print(res.headers['Date'])
+# print(res.text)
+
+data = requests.get(
+    'https://opentdb.com/api.php?amount=1&category=18&difficulty=easy&type=multiple')
+
+print(data.text)

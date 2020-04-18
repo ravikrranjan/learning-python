53b5bcf38e7405ffcbfba2b0cdba14fb0ffd7cec
ravikrranjan <raviranjan1600@gmail.com>
2020-04-18 21:46:26 +0530

    python keyword

---
+++ python_example/__keyword.py
@@ -0,0 +1,8 @@
+import keyword
+
+# Keywords are the reserved words in Python.
+# #We cannot use a keyword as variable name, function name or any other identifier.
+print(keyword.iskeyword)
+
+# keyword list
+print(keyword.kwlist)

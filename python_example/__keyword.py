ffe64b527919b205aec6c271928e1bad76011e88
ravikrranjan <raviranjan1600@gmail.com>
2020-04-18 21:47:46 +0530

    python keyword

--- python_example/__keyword.py
+++ python_example/__keyword.py
@@ -4,5 +4,11 @@ import keyword
 # #We cannot use a keyword as variable name, function name or any other identifier.
 print(keyword.iskeyword)
 
+# Result : <built-in method __contains__ of frozenset object at 0x1033f8580 >
+
 # keyword list
 print(keyword.kwlist)
+
+# Result :
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally',
+#    'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']

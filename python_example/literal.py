589f13e29712d61c059f09ace6b184ccd0f6c6d3
ravikrranjan <raviranjan1600@gmail.com>
2020-04-21 08:16:53 +0530

    literal and constant

---
+++ python_example/literal.py
@@ -0,0 +1,20 @@
+fruits = ["apple", "mango", "orange"]  # list
+numbers = (1, 2, 3)  # tuple
+alphabets = {'a': 'apple', 'b': 'ball', 'c': 'cat'}  # dictionary
+vowels = {'a', 'e', 'i', 'o', 'u'}  # set
+
+print(fruits)
+print(numbers)
+print(alphabets)
+print(vowels)
+
+
+print(type(fruits))
+print(type(numbers))
+print(type(alphabets))
+print(type(vowels))
+
+# <class 'list' >
+# <class 'tuple' >
+# <class 'dict' >
+# <class 'set' >
---
+++ python_example/main.py
@@ -0,0 +1,9 @@
+import constant
+
+constant.PI = 6.8
+print(constant.PI)
+
+print(constant.GRAVITY)
+
+print(type(constant.GRAVITY))
+print(type(constant.PI))

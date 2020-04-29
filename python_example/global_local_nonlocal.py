523ec3541ce6b70046c4fd4a0524080115bc3b35
ravikrranjan <raviranjan1600@gmail.com>
2020-04-29 07:41:49 +0530

    local nonlocal

--- python_example/global_local_nonlocal.py
+++ python_example/global_local_nonlocal.py
@@ -1,10 +1,52 @@
-x = "global"
+# 1.
+# x = "global"
+# def foo():
+#     print("x inside :", x)
+# foo()
+# print("x outside:", x)
+
+# 2.
+# x = 2
+# def double():
+#     x = x * 2
+#     print(x)
 
 
-def foo():
-    print("x inside :", x)
+# print(double())  # UnboundLocalError: local variable 'x' referenced before assignment
 
 
-foo()
+# 3.
 
-print("x outside:", x)
+# x = "global"
+
+
+# def foo():
+#     # global x
+#     y = "local"
+#     # x = x * 2
+#     print(x * 2)
+#     print(y * 2)
+
+
+# foo()
+
+# 4.
+# Create a nonlocal variable
+
+
+def outer():
+    x = "local"
+
+    def inner():
+        nonlocal x
+        x = "nonlocal"
+        print("inner:", x)
+        x = x * 2
+        print("double:", x)
+
+    inner()
+
+    print("outer:", x)
+
+
+outer()
